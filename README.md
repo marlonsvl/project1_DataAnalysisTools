466312d7744a058ce9e9b8272cab7e74289cbb60
Marlon <marsantovi@gmail.com>
2015-10-28 16:25:10 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+#Project 1 of Data Analysis Tools
+
+##Data
+I choose National Epidemiologic Survey of Drug Use and Health Code Book
+
+https://d396qusza40orc.cloudfront.net/phoenixassets/data-management-visualization/NESARC%20Wave%201%20Code%20Book%20w%20toc.pdf
+
+###Explanatory Variable
+
+OCCUPATION: CURRENT OR MOST RECENT JOB ( 15 level categorical)
+
+###Response Variable
+
+NUMBER OF EPISODES OF ALCOHOL ABUSE (Quantitative from 20 to 99 ) 
+
+###Description
+
+I choose python to make ANOVA with 15 categorical levels Analysis and  show me the following results:
+
+![alt tag](https://github.com/marlonsvl/project1_DataAnalysisTools/blob/master/images/img1.png) 
+
+
+
+
+
+
---
+++ anova_nsarc.py
@@ -0,0 +1,47 @@
+import numpy
+import pandas
+import statsmodels.formula.api as smf
+import statsmodels.stats.multicomp as multi 
+
+
+data = pandas.read_csv('/Users/utpl/Documents/DataAnalysisTools/project1_DataAnalysisTools/nesarc_pds.csv', low_memory=False)
+
+
+#AMERICAN INDIAN OR ALASKA NATIVE Yes = 1 No = 2
+#sub1_indianAlaska = data[(data['S1Q1D1']==1)]
+
+#HISPANIC OR LATINO ORIGIN
+#sub2_latino = data[(data['S1Q1C']==1)]
+
+#ASIAN
+#sub3_asian = data[(data['S1Q1D2']==1)]
+
+
+#BLACK OR AFRICAN AMERICAN
+#sub4_african = data[(data['S1Q1D3']==1)]
+
+
+
+#NUMBER OF EPISODES OF ALCOHOL ABUSE
+
+data['S2BQ3B'] = data['S2BQ3B'].replace('BL', numpy.nan)
+data['S2BQ3B'] = data['S2BQ3B'].replace(' ', 0) 
+
+data['S2BQ3B'] = pandas.to_numeric(data['S2BQ3B'])
+
+sub1 = data[(data['S2BQ3B'] >= 20) & (data['S2BQ3B'] < 99)]
+
+#NUMBER OF EPISODES OF ALCOHOL ABUSE and OCCUPATION: CURRENT OR MOST RECENT JOB
+sub2 = sub1[['S2BQ3B', 'S1Q9B']].dropna()
+
+
+model1 = smf.ols(formula='S2BQ3B ~ C(S1Q9B)', data=sub2).fit()
+print (model1.summary())
+
+
+
+mc1 = multi.MultiComparison(sub2['S2BQ3B'], sub2['S1Q9B'])
+res1 = mc1.tukeyhsd()
+print(res1.summary())
+
+
